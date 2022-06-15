7a3ac6fa4417b71790b9c09a1b60e57a54d3add1
copokbl <zaneharrison2006@icloud.com>
2022-06-15 16:20:11 +1000

    First commit

---
+++ FullGui.cs
@@ -0,0 +1,89 @@
+using System.Text;
+using SimpleChatAppLibrary;
+
+namespace SimpleChatAppClient; 
+
+public static class FullGui {
+    
+    private static string _currentlyTypedText = "";
+    private static IEnumerable<SimpleChatAppMessage>? _lastMessages;
+    private static SimpleChatAppLibrary.SimpleChatAppClient _client;
+
+    public static void Start(SimpleChatAppLibrary.SimpleChatAppClient client) {
+        _client = client;
+        new Thread(ReaderThread).Start();
+        
+        while (true) {
+            string msg = PasswordTyping();
+            _currentlyTypedText = "";
+            try {
+                _client.SendMessage(msg);
+            }
+            catch (Exception e) {
+                Console.WriteLine("Error sending message: " + e.Message);
+            }
+        }
+        
+    }
+
+    private static void ReaderThread() {
+        Console.Title = "Simple Chat App - Message Reader";
+        while (true) {
+            try {
+                Cycle(_client);
+            }
+            catch (Exception e) {
+                Console.Clear();
+                Console.WriteLine("Cycle Failed: " + e.Message);
+                Console.WriteLine("trying again in 10 seconds...");
+                Thread.Sleep(TimeSpan.FromSeconds(9));
+            }
+            Thread.Sleep(TimeSpan.FromSeconds(0.5));
+        }
+    }
+    
+    private static void Cycle(SimpleChatAppLibrary.SimpleChatAppClient client) {
+        // Get messages from server
+        IEnumerable<SimpleChatAppMessage> messages = client.GetMessages(15);
+        // If there are no new messages, return
+        if (_lastMessages != null && _lastMessages.Equals(messages)) {
+            return;
+        }
+        _lastMessages = messages;
+        Console.Clear();
+        foreach (SimpleChatAppMessage msg in messages) {
+            Console.WriteLine($"{DateTime.FromBinary(msg.createdAt).ToLocalTime()}\n" +
+                              $"{msg.creatorName}: {msg.text}\n");
+        }
+        Console.Write("Message: " + _currentlyTypedText);
+    }
+    
+    private static string PasswordTyping() {
+        StringBuilder pass = new ();
+            
+        while (true) {
+            ConsoleKeyInfo ki = Console.ReadKey(true);
+
+            if (ki.Key == ConsoleKey.Enter) break;
+            if (ki.Key == ConsoleKey.Backspace) {
+                if (pass.Length < 1) continue;
+
+                pass.Remove(pass.Length - 1, 1);
+                Console.SetCursorPosition(Console.CursorLeft - 1, Console.CursorTop);
+                Console.Write(" ");
+                Console.SetCursorPosition(Console.CursorLeft - 1, Console.CursorTop);
+                _currentlyTypedText = pass.ToString();
+                continue;
+            }
+            
+            Console.Write(ki.KeyChar);
+
+            pass.Append(ki.KeyChar);
+            _currentlyTypedText = pass.ToString();
+        }
+        Console.Write("\n");
+
+        return pass.ToString();
+    }
+    
+}
---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿
+using SimpleChatAppClient;
+
+Console.WriteLine("Starting...");
+
+// Ask for channel
+Console.Write("Channel: ");
+string channel = Console.ReadLine();
+
+// Ask for username
+Console.Write("Username: ");
+string username = Console.ReadLine();
+
+SimpleChatAppLibrary.SimpleChatAppClient client = new("http://chat.serble.net", username, channel);
+
+if (!client.TestConnection()) {
+    Console.WriteLine("Connection failed.");
+    return 1;
+}
+Console.WriteLine("Connection successful.");
+
+FullGui.Start(client);
+
+return 0;
